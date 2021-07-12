376c99df20e661c0adc4fbf383e4a62a70738b2d
Davino Liessens <davino.liessens@hotmail.com>
2021-07-12 19:42:49 +0200

    Created File for Dependency Injection with Db

---
+++ BirdAPI/DAL/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<IBirdRepository, BirdRepository>();
+            services.AddTransient<IOwnerRepository, OwnerRepository>();
+            return services;
+        }
+
+        public static IServiceCollection RegisterORM(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+            return services;
+        }
+    }
+}
