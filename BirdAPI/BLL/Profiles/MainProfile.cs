d5e5c8fbbfa46f1998f0808122b161a16ba3957c
Davino Liessens <davino.liessens@hotmail.com>
2021-07-12 19:44:43 +0200

    Created profiles to use with Mapper

---
+++ BirdAPI/BLL/Profiles/MainProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BLL.ViewModels;
+using DAL.DB_Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Profiles
+{
+    public class MainProfile : Profile
+    {
+        public MainProfile()
+        {
+            // BIRD
+            CreateMap<Bird, BirdVM>();
+            CreateMap<CreateBirdVM, Bird>();
+            CreateMap<ChangeBirdVM, Bird>();
+            CreateMap<ChangeBirdVM, BirdVM>();
+            CreateMap<BirdVM, CreateBirdVM>();
+
+            // OWNER
+            CreateMap<Owner, OwnerVM>();
+            CreateMap<CreateOwnerVM, Owner>();
+            CreateMap<ChangeOwnerVM, Owner>();
+            CreateMap<ChangeOwnerVM, OwnerVM>();
+        }
+    }
+}
