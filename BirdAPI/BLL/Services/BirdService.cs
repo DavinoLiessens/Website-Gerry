646a83692454a850a3b2e653d09d837cb04e7cdf
Davino Liessens <davino.liessens@hotmail.com>
2021-07-12 19:44:14 +0200

    Created Services for the Db

---
+++ BirdAPI/BLL/Services/BirdService.cs
@@ -0,0 +1,109 @@
+﻿using AutoMapper;
+using BLL.ViewModels;
+using DAL.DB_Models;
+using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public interface IBirdService
+    {
+        Task<BirdVM> GetBird(int id);
+        Task<List<BirdVM>> GetAllBirds();
+        Task<BirdVM> CreateBird(CreateBirdVM body);
+        Task<BirdVM> DeleteBird(int id);
+        Task<BirdVM> ChangeBird(int id, ChangeBirdVM body);
+    }
+
+    public class BirdService : IBirdService
+    {
+        private readonly IMapper _mapper;
+        private readonly IBirdRepository _repo;
+
+        public BirdService(IMapper mapper, IBirdRepository repo)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        public async Task<BirdVM> ChangeBird(int id, ChangeBirdVM body)
+        {
+            Bird bird = await _repo.GetBird(id);
+
+            if (body.Ringnummer != null)
+                bird.Ringnummer = body.Ringnummer;
+            //if (body.Geslacht != null)
+            //    bird.Geslacht = body.Geslacht;
+            //if (body.Jaartal != null)
+            //    bird.Jaartal = body.Jaartal;
+            if (body.Kotnummer != null)
+                bird.Kotnummer = body.Kotnummer;
+            //if (body.Soort != null)
+            //    bird.Soort = body.Soort;
+            //if (body.OwnerFullName != null)
+            //    bird.Eigenaar.Voornaam = body.OwnerFullName.Substring(0, body.OwnerFullName.IndexOf(" "));
+            //    bird.Eigenaar.Achternaam = body.OwnerFullName.Substring(body.OwnerFullName.IndexOf(" "), body.OwnerFullName.Length);
+
+            bird = await _repo.ChangeBird(bird);
+            BirdVM viewmodel = _mapper.Map<BirdVM>(bird);
+            viewmodel.OwnerFullName = $"{bird.Eigenaar.Voornaam} {bird.Eigenaar.Achternaam}";
+            return viewmodel;
+        }
+
+        public async Task<BirdVM> CreateBird(CreateBirdVM body)
+        {
+            Bird bird = _mapper.Map<Bird>(body);
+            bird = await _repo.CreateBird(bird);
+
+            //CreateBirdVM viewmodel = new CreateBirdVM();
+            //viewmodel.owner.Voornaam = bird.Eigenaar.Voornaam;
+            //viewmodel.owner.Achternaam = bird.Eigenaar.Achternaam;
+
+            //return _mapper.Map<BirdVM>(viewmodel);
+            return _mapper.Map<BirdVM>(bird);
+        }
+
+        public async Task<BirdVM> DeleteBird(int id)
+        {
+            Bird bird = await _repo.DeleteBird(id);
+            BirdVM viewmodel = _mapper.Map<BirdVM>(bird);
+            return viewmodel;
+        }
+
+        public async Task<List<BirdVM>> GetAllBirds()
+        {
+            List<Bird> bird = await _repo.GetAllBirds();
+            List<BirdVM> viewmodel = _mapper.Map<List<BirdVM>>(bird);
+            int i = 0;
+            foreach(var item in viewmodel)
+            {
+                Bird newBird = bird[i];
+                item.OwnerFullName = $"{newBird.Eigenaar.Voornaam} {newBird.Eigenaar.Achternaam}";
+                i++;
+            }
+            i = 0;
+
+            return viewmodel;
+        }
+
+        public async Task<BirdVM> GetBird(int id)
+        {
+            Bird dbModel = await _repo.GetBird(id);
+            BirdVM viewModel = _mapper.Map<BirdVM>(dbModel);
+            viewModel.OwnerFullName = $"{dbModel.Eigenaar.Voornaam} {dbModel.Eigenaar.Achternaam}"; 
+            //BirdVM viewModel = new BirdVM
+            //{
+            //    ID = dbModel.ID,
+            //    Ringnummer = dbModel.Ringnummer,
+            //    Geslacht = dbModel.Geslacht,
+            //    Soort = dbModel.Soort,
+            //    Jaartal = dbModel.Jaartal,
+            //    Kotnummer = dbModel.Kotnummer
+            //};
+            return viewModel;
+        }
+    }
+}
---
+++ BirdAPI/BLL/Services/OwnerService.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using BLL.ViewModels;
+using DAL.DB_Models;
+using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public interface IOwnerService
+    {
+        Task<OwnerVM> GetOwner(int id);
+        Task<List<OwnerVM>> GetAllOwners();
+        Task<OwnerVM> CreateOwner(CreateOwnerVM body);
+        Task<OwnerVM> DeleteOwner(int id);
+        Task<OwnerVM> ChangeOwner(int id, ChangeOwnerVM body);
+    }
+
+    public class OwnerService : IOwnerService
+    {
+        private readonly IMapper _mapper;
+        private readonly IOwnerRepository _repo;
+
+        public OwnerService(IOwnerRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        public async Task<OwnerVM> ChangeOwner(int id, ChangeOwnerVM body)
+        {
+            Owner owner = await _repo.GetOwner(id);
+
+            if (body.Voornaam != null)
+                owner.Voornaam = body.Voornaam;
+            if (body.Achternaam != null)
+                owner.Achternaam = body.Achternaam;
+
+            owner = await _repo.ChangeOwner(owner);
+
+            return _mapper.Map<OwnerVM>(owner);
+        }
+
+        public async Task<OwnerVM> CreateOwner(CreateOwnerVM body)
+        {
+            Owner owner = _mapper.Map<Owner>(body);
+            owner = await _repo.CreateOwner(owner);
+            return _mapper.Map<OwnerVM>(owner);
+        }
+
+        public async Task<OwnerVM> DeleteOwner(int id)
+        {
+            Owner owner = await _repo.DeleteOwner(id);
+            OwnerVM viewmodel = _mapper.Map<OwnerVM>(owner);
+            return viewmodel;
+        }
+
+        public async Task<List<OwnerVM>> GetAllOwners()
+        {
+            List<Owner> owner = await _repo.GetAllOwners();
+            List<OwnerVM> viewmodel = _mapper.Map<List<OwnerVM>>(owner);
+            return viewmodel;
+        }
+
+        public async Task<OwnerVM> GetOwner(int id)
+        {
+            Owner owner = await _repo.GetOwner(id);
+            OwnerVM viewmodel = _mapper.Map<OwnerVM>(owner);
+            //OwnerVM viewmodel = new OwnerVM()
+            //{
+            //    ID = owner.ID,
+            //    Voornaam = owner.Voornaam,
+            //    Achternaam = owner.Achternaam
+            //};
+
+            return viewmodel;
+        }
+    }
+}
