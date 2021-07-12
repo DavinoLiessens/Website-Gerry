f8eb97cc7310b6cbed823b0423ce1ac38894c4d7
Davino Liessens <davino.liessens@hotmail.com>
2021-07-12 19:42:02 +0200

    Created AppDbContext for Db

---
+++ BirdAPI/DAL/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using DAL.DB_Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Bird> Birds { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Bird>()
+                // 1 Bird has 1 owner
+                .HasOne(x => x.Eigenaar)
+                // 1 Owner has many birds
+                .WithMany(x => x.Vogels)
+                .HasForeignKey(x => x.EigenaarID)
+                .IsRequired(true)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
