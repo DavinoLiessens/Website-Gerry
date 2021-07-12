b30fc3b14384af8c04cf7e046ea902eac9423f4b
Davino Liessens <davino.liessens@hotmail.com>
2021-07-12 19:41:45 +0200

    Created Repositories for the Db

---
+++ BirdAPI/DAL/Repositories/BirdRepository.cs
@@ -0,0 +1,61 @@
+﻿using DAL.DB_Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public interface IBirdRepository
+    {
+        Task<Bird> GetBird(int id);
+        Task<List<Bird>> GetAllBirds();
+        Task<Bird> CreateBird(Bird bird);
+        Task<Bird> DeleteBird(int id);
+        Task<Bird> ChangeBird(Bird bird);
+    }
+
+    public class BirdRepository : IBirdRepository
+    {
+        private readonly AppDbContext _context;
+
+        public BirdRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Bird> ChangeBird(Bird bird)
+        {
+            Bird newBird = await _context.Birds.FindAsync(bird.ID);
+            _context.Birds.Update(newBird);
+            await _context.SaveChangesAsync();
+            return newBird;
+        }
+
+        public async Task<Bird> CreateBird(Bird bird)
+        {
+            Bird newBird = _context.Birds.Add(bird).Entity;
+            await _context.SaveChangesAsync();
+            return newBird;
+        }
+
+        public async Task<Bird> DeleteBird(int id)
+        {
+            Bird bird = await _context.Birds.FindAsync(id);
+            _context.Birds.Remove(bird);
+            await _context.SaveChangesAsync();
+            return bird;
+        }
+
+        public async Task<List<Bird>> GetAllBirds()
+        {
+            return await _context.Birds.Include(x => x.Eigenaar).ToListAsync();
+        }
+
+        public async Task<Bird> GetBird(int id)
+        {
+            return await _context.Birds.Include(x => x.Eigenaar).SingleOrDefaultAsync(x => x.ID == id);
+        }
+    }
+}
---
+++ BirdAPI/DAL/Repositories/OwnerRepository.cs
@@ -0,0 +1,61 @@
+﻿using DAL.DB_Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public interface IOwnerRepository
+    {
+        Task<List<Owner>> GetAllOwners();
+        Task<Owner> GetOwner(int id);
+        Task<Owner> CreateOwner(Owner owner);
+        Task<Owner> DeleteOwner(int id);
+        Task<Owner> ChangeOwner(Owner owner);
+    }
+
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly AppDbContext _context;
+
+        public OwnerRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Owner> ChangeOwner(Owner owner)
+        {
+            Owner newOwner = await _context.Owners.FindAsync(owner.ID);
+            _context.Owners.Update(newOwner);
+            await _context.SaveChangesAsync();
+            return newOwner;
+        }
+
+        public async Task<Owner> CreateOwner(Owner owner)
+        {
+            Owner newOwner = _context.Owners.Add(owner).Entity;
+            await _context.SaveChangesAsync();
+            return newOwner;
+        }
+
+        public async Task<Owner> DeleteOwner(int id)
+        {
+            Owner owner = await _context.Owners.FindAsync(id);
+            _context.Owners.Remove(owner);
+            await _context.SaveChangesAsync();
+            return owner;
+        }
+
+        public async Task<List<Owner>> GetAllOwners()
+        {
+            return await _context.Owners.ToListAsync();
+        }
+
+        public async Task<Owner> GetOwner(int id)
+        {
+            return await _context.Owners.FindAsync(id);
+        }
+    }
+}
