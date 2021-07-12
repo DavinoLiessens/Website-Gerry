396e0ff0f145582e8ce536355d3c13e84c024ddf
Davino Liessens <davino.liessens@hotmail.com>
2021-07-12 19:47:49 +0200

    Created Controllers for the Db

---
+++ BirdAPI/BirdAPI/Controllers/BirdsController.cs
@@ -0,0 +1,57 @@
+﻿using BLL.Services;
+using BLL.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirdAPI.Controllers
+{
+    [Route("api/v1/birds")]
+    [ApiController]
+    public class BirdsController : Controller
+    {
+        private readonly IBirdService _birdService;
+
+        public BirdsController(IBirdService birdService)
+        {
+            _birdService = birdService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBird(int id)
+        {
+            BirdVM vm = await _birdService.GetBird(id);
+            return Ok(vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBirds()
+        {
+            List<BirdVM> vm = await _birdService.GetAllBirds();
+            return Ok(vm);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> ChangeBird(int id, [FromBody] ChangeBirdVM body)
+        {
+            BirdVM vm = await _birdService.ChangeBird(id, body);
+            return Ok(vm);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBird(int id)
+        {
+            BirdVM vm = await _birdService.DeleteBird(id);
+            return Ok(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBird([FromBody] CreateBirdVM body)
+        {
+            BirdVM vm = await _birdService.CreateBird(body);
+            return Ok(vm);
+        }
+    }
+}
---
+++ BirdAPI/BirdAPI/Controllers/OwnersController.cs
@@ -0,0 +1,55 @@
+﻿    using BLL.Services;
+using BLL.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirdAPI.Controllers
+{
+    [Route("api/v1/owners")]
+    [ApiController]
+    public class OwnersController : Controller
+    {
+        private readonly IOwnerService _ownerService;
+        public OwnersController(IOwnerService ownerService)
+        {
+            _ownerService = ownerService;
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOwner(int id)
+        {
+            OwnerVM vm = await _ownerService.GetOwner(id);
+            return Ok(vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllOwners()
+        {
+            List<OwnerVM> vm = await _ownerService.GetAllOwners();
+            return Ok(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOwner([FromBody] CreateOwnerVM body)
+        {
+            OwnerVM vm = await _ownerService.CreateOwner(body);
+            return Ok(vm);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOwner(int id)
+        {
+            OwnerVM vm = await _ownerService.DeleteOwner(id);
+            return Ok(vm);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> ChangeOwner(int id, [FromBody] ChangeOwnerVM body)
+        {
+            OwnerVM vm = await _ownerService.ChangeOwner(id, body);
+            return Ok(vm);
+        }
+    }
+}
