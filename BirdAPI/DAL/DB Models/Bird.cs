6ed4e76ec1b590494559b3d75c947afddc51c065
Davino Liessens <davino.liessens@hotmail.com>
2021-07-12 19:41:29 +0200

    Created DbModels

---
+++ BirdAPI/DAL/DB Models/Bird.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.DB_Models
+{
+    public class Bird
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public int? Ringnummer { get; set; }
+        [Required]
+        public string Geslacht { get; set; }
+        [Required]
+        public string Soort { get; set; }
+        [Required]
+        public int? Jaartal { get; set; }
+        [Required]
+        public int? Kotnummer { get; set; }
+        public int EigenaarID { get; set; }
+        [Required]
+        public Owner Eigenaar { get; set; }
+    }
+}
---
+++ BirdAPI/DAL/DB Models/Owner.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.DB_Models
+{
+    public class Owner
+    {
+        public int ID { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public ICollection<Bird> Vogels { get; set; }
+    }
+}
