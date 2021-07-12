a14405a13e1aadd5e05553ed7c5d1150d8cd780a
Davino Liessens <davino.liessens@hotmail.com>
2021-07-12 19:45:01 +0200

    Created file for Dependency Injection with Db

---
+++ BirdAPI/BLL/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using BLL.Profiles;
+using BLL.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            // AUTOMAPPER
+            services.AddAutoMapper(typeof(MainProfile));
+
+            // SERVICES
+            services.AddTransient<IBirdService, BirdService>();
+            services.AddTransient<IOwnerService, OwnerService>();
+
+            return services;
+        }
+    }
+}
