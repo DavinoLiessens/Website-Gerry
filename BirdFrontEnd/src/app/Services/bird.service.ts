4992406993df33cf5aa6ef7694fca18dec1e184d
Davino Liessens <davino.liessens@hotmail.com>
2021-07-16 17:43:38 +0200

    Added BirdService

--- BirdFrontEnd/src/app/Services/bird.service.ts
+++ BirdFrontEnd/src/app/Services/bird.service.ts
@@ -1,31 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ApiService, Bird } from './api.service';
 @Injectable({
   providedIn: 'root'
 })
 export class BirdService {
 
-  constructor(private _http: HttpClient) { }
+  public Birds: Bird[];
+  private searchName: string;
 
-  GetAllBirds(){
-      return this._http.get("http://localhost:4000/api/v1/birds");
+  constructor(private _http: HttpClient, private apiService: ApiService) { }
+
+  get SearchName(){
+    return this.searchName;
   }
 
-}
+  set SearchName(value: string){
+    this.searchName = value;
+    this.apiService.GetAllBirds(this.searchName);
+  }
 
-export interface IBird{
-  id: number;
-  ringnumber: number;
-  geslacht: string;
-  typeOfBird: string;
-  age: number;
-  cageNumber: number;
-  ownerID: number;
-  owner: IOwner;
-}
+  GetAllBirds(){
+      this.Birds = [];
+      return this.apiService.GetAllBirds(this.searchName);
+  }
 
-export interface IOwner{
-  id: number;
-  firstname: string;
-  lastname: string;
+  GetBird(id: number){
+    return this.apiService.GetBird(id);
+  }
+
+  CreateBird(newBird: Bird){
+    return this.apiService.CreateBird(newBird);
+  }
+
+  UpdateBird(updateBird: Bird){
+    return this.apiService.UpdateBird(updateBird);
+  }
+
+  DeleteBird(id: number){
+    return this.apiService.DeleteBird(id);
+  }
 }
