2a18326c13fb21698806a666f242e31bd4ddf3c4
Davino Liessens <davino.liessens@hotmail.com>
2021-07-22 14:16:03 +0200

    Added Database to view all the Owners

--- BirdFrontEnd/src/app/Services/owner.service.ts
+++ BirdFrontEnd/src/app/Services/owner.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { ApiService, Owner } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OwnerService {
 
-  constructor() { }
+  public Owners: Owner[];
+  private searchName: string;
+
+  public noOwners: number = this.apiService.noOwners;
+  public sortItemOwners: string = this.apiService.sortItemOwners;
+
+  constructor(private apiService: ApiService) { }
+
+  get SearchName(){
+    return this.searchName;
+  }
+
+  set SearchName(value: string){
+    this.searchName = value;
+    this.apiService.GetAllOwners(this.searchName);
+  }
+
+  GetAllOwners(){
+    this.Owners = [];
+    return this.apiService.GetAllOwners();
+  }
+
+  GetOwner(id: number){
+    return this.apiService.GetOwner(id);
+  }
+
+  CreateOwner(owner: Owner){
+    return this.apiService.CreateOwner(owner);
+  }
+
+  UpdateOwner(owner: Owner){
+    return this.apiService.UpdateOwner(owner);
+  }
+
+  DeleteOwner(id: number){
+    return this.apiService.DeleteOwner(id);
+  }
 }
--- BirdFrontEnd/src/app/owner/owner.component.ts
+++ BirdFrontEnd/src/app/owner/owner.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService, Owner } from '../Services/api.service';
+import { OwnerService } from '../Services/owner.service';
 
 @Component({
   selector: 'app-owner',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OwnerComponent implements OnInit {
 
-  constructor() { }
+  owners: Owner[];
+  items: number[];
+  sortItems: string[];
+
+  constructor(private apiService: ApiService, private ownerService: OwnerService) { 
+    this.items = [5,10,15,20,25,50,100];
+    this.sortItems = ["Alles", "Voornaam"];
+  }
+
+  public noOwners: number = this.ownerService.noOwners;
+  public sortItemOwners: string = this.ownerService.sortItemOwners;
 
   ngOnInit(): void {
+    this.GetAllOwners();
+  }
+
+  GetAllOwners(){
+    try{
+      console.log("Eigenaars ophalen.");
+      this.ownerService.GetAllOwners().subscribe((res) => this.owners = res);
+    }
+    catch{
+      console.log("Er was eenprobleem.");
+    }
+  }
+
+  get SearchName(){
+    return this.ownerService.SearchName;
+  }
+
+  set SearchName(value: string){
+    this.ownerService.SearchName = value;
+    this.apiService.GetAllOwners(value).subscribe((res) => this.owners = res);
+  }
+
+  get NoOwners(){
+    return this.ownerService.noOwners;
+  }
+
+  set NoOwners(value: number){
+    this.ownerService.noOwners = value;
+    this.apiService.GetAllOwners().subscribe((res) => this.owners = res);
   }
 
 }
